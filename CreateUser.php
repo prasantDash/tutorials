3f20eaa6ecc099cd1f5958757d3b3967b220f496
Dash <930030@cognizant.com>
2022-09-02 16:14:48 +0530

    test commit testCreateUser funtion added

--- CreateUser.php
+++ CreateUser.php
@@ -3,5 +3,8 @@ class CreateUser{
     public function __construct($DBConn){
         $DBConn->createConnection();               
     }
+    public function testCreateUser(){
+        echo "Test User";
+    }
 }
 ?>
